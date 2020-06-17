bc36058f5e00361e2add7977eac1444192bfae08
waleedalbahar <waleedalbahar10@gmail.com>
2020-06-17 21:24:05 +0300

    classwork

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,22 @@
+
+
+let family = ["Abdullah", "Waleed", "Mohammed", "Ahmad", "Fares"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [ 98.6, 99.1, 94.3]
+
+var trueFalse = [ true, true, true, false]
+
+print(family[0])
+      (family[4])
+
+ print(evens[0])
+       (evens[5])
+
+print(grades[0])
+      (grades[2])
+
+print(trueFalse[0])
+     ( trueFalse[3])
+
---
+++ ios-cw2-p3.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+
+var grades = [90.4, 83.92, 90.3]
+let average = (grades[0] + grades[1] + grades[2]) / 3
+
+if average >= 90{
+  print("ممتاز")
+}
+else if average >= 80{
+    print("جيد جداً")
+}
+else if average >= 70{
+    print("جيد")
+}
+else{
+    print("راسب")
+}
+grades.remove(at:1)
+
+let average2 = (grades[0] +  grades[1]) / 3
+
+if average2 >= 90{
+  print("ممتاز")
+}
+else if average2 >= 80{
+    print("جيد جداً")
+}
+else if average2 >= 70{
+    print("جيد")
+}
+else{
+    print("راسب")
+}
